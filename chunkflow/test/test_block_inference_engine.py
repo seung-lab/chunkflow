a10ace7469145f97fdbde7b9c9a9655a52e7d34c
jingpengw <jingpeng.wu@gmail.com>
2019-02-07 11:42:49 -0500

    input image is not aligned with patch size and patch overlap

--- chunkflow/test/test_block_inference_engine.py
+++ chunkflow/test/test_block_inference_engine.py
@@ -6,7 +6,7 @@ from chunkflow.offset_array import OffsetArray
 
 
 class TestBlockInferenceEngine(unittest.TestCase):
-    def test_block_inference_engine(self):
+    def test_aligned_input_chunk(self):
         print('\ntest block inference engine...')
         patch_engine = IdentityPatchInferenceEngine()
         block_inference_engine = BlockInferenceEngine(
@@ -18,13 +18,35 @@ class TestBlockInferenceEngine(unittest.TestCase):
 
         image = np.random.rand(28 * 2 + 4, (256 - 64) * 2 + 64,
                                (256 - 64) * 2 + 64)
-        image = np.asarray(image, dtype='float32')
+        image = np.asarray(image, dtype=np.float32)
         image = OffsetArray(image)
         output = block_inference_engine(image)
         output = np.reshape(output, image.shape)
 
         # some of the image voxel is 0, the test can only work with rtol=1
         np.testing.assert_allclose(image, output, rtol=1, atol=1e-17)
+    
+    def test_non_aligned_input_chunk(self):
+        print('\ntest block inference engine with non aligned input chunk...')
+        patch_engine = IdentityPatchInferenceEngine()
+        block_inference_engine = BlockInferenceEngine(
+            patch_inference_engine=patch_engine,
+            patch_size=(32, 256, 256),
+            patch_overlap=(4, 64, 64),
+            num_output_channels=1,
+            is_padding = True
+        )
+
+        image = np.random.rand(28 * 2 + 3, (256 - 64) * 2 + 62,
+                               (256 - 64) * 2 + 60)
+        image = np.asarray(image, dtype=np.float32)
+        image = OffsetArray(image)
+        output = block_inference_engine(image)
+        output = np.reshape(output, image.shape)
+
+        # some of the image voxel is 0, the test can only work with rtol=1
+        np.testing.assert_allclose(image, output, rtol=1, atol=1e-17)
+
 
 
 if __name__ == '__main__':
