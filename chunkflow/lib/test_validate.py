9799f742a4d5f0a4e439254c125a8bf04944c81d
jingpengw <jingpeng.wu@gmail.com>
2019-02-27 10:17:35 -0500

    add a new parameter to control the sensitivity of template matching, previously it requires perfect matching

--- chunkflow/lib/test_validate.py
+++ chunkflow/lib/test_validate.py
@@ -6,11 +6,10 @@ from .validate import validate_by_template_matching
 
 class TestValidateByTemplateMatching(unittest.TestCase):
     def test_validate_by_template_matching(self):
+        print('test validate by template matching...')
         image = np.random.randint(0, 256, size=(64, 64, 64), dtype=np.uint8)
         assert validate_by_template_matching(image)
         
-        # make a black box 
+        # make a black box
         image[16:-16, 16:-16, 16:-16] = 0
         assert not validate_by_template_matching(image)
-    
-
--- chunkflow/lib/validate.py
+++ chunkflow/lib/validate.py
@@ -4,8 +4,8 @@ from warnings import warn
 from skimage.feature import match_template
 
 
-def validate_by_template_matching(img):
-    """ detect 3d black boxes by template matching.
+def validate_by_template_matching(img, verbose=True):
+    """ Detect 3d black boxes by template matching.
     1. binarize the image. the voxels inside the black box will be false, and the outside will be true
     2. The template is 7x7x2 with one section true and the other false. 
     3. sliding the template through the array, and detect the matching regions. 
@@ -14,54 +14,56 @@ def validate_by_template_matching(img):
     Note that this is always effective. If the black box is large enough to reach both sides, 
     the detection will fail.
     """
-    print("testing validation by template matching...")
+    if verbose:
+        print("validation by template matching...")
+
     if np.issubdtype(img.dtype, np.floating):
         warn('do not support image with floating data type, will skip the validation.')
         return True
-    elif np.issubdtype(img.dtype, np.integer):
-        img = img.astype(dtype=np.bool)
-    assert img.dtype == np.bool
-    matching_threshold = 100
+    
+    img = img.astype(dtype=np.bool)
+
+    score_threshold = 0.9
+    num_threshold = 100
     evidence_point = 0
 
     temp = np.zeros((7, 7, 2), dtype=np.bool)
     temp[:, :, 0] = True
     result = match_template(img, temp)
-    if np.count_nonzero(result == 1) > matching_threshold:
+    if np.count_nonzero(result > score_threshold) > num_threshold:
         evidence_point += 1
 
     temp = np.zeros((7, 7, 2), dtype=np.bool)
     temp[:, :, 1] = True
     result = match_template(img, temp)
-    if np.count_nonzero(result == 1) > matching_threshold:
+    if np.count_nonzero(result > score_threshold) > num_threshold:
         evidence_point += 1
 
     temp = np.zeros((2, 7, 7), dtype=np.bool)
     temp[0, :, :] = True
     result = match_template(img, temp)
-    if np.count_nonzero(result == 1) > matching_threshold:
+    if np.count_nonzero(result > score_threshold) > num_threshold:
         evidence_point += 1
 
     temp = np.zeros((2, 7, 7), dtype=np.bool)
     temp[1, :, :] = True
     result = match_template(img, temp)
-    if np.count_nonzero(result == 1) > matching_threshold:
+    if np.count_nonzero(result > score_threshold) > num_threshold:
         evidence_point += 1
 
     temp = np.zeros((7, 2, 7), dtype=np.bool)
     temp[:, 0, :] = True
     result = match_template(img, temp)
-    if np.count_nonzero(result == 1) > matching_threshold:
+    if np.count_nonzero(result > score_threshold) > num_threshold:
         evidence_point += 1
 
     temp = np.zeros((7, 2, 7), dtype=np.bool)
     temp[:, 1, :] = True
     result = match_template(img, temp)
-    if np.count_nonzero(result == 1) > matching_threshold:
+    if np.count_nonzero(result > score_threshold) > num_threshold:
         evidence_point += 1
 
     if evidence_point > 4:
         return False
     else:
         return True
-
