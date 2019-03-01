f3e8c1d7a1410672fa542bb46987c001624bcb2c
Jingpeng Wu <jingpeng.wu@gmail.com>
2019-03-01 17:47:59 -0500

    separate cloud watch as separate operator; fix travis test (#42)

--- chunkflow/operators/upload_log.py
+++ chunkflow/operators/upload_log.py
@@ -1,7 +1,6 @@
 from .operator_base import OperatorBase
 import json
 from cloudvolume import Storage
-from chunkflow.aws.cloud_watch import CloudWatch
 
 
 class UploadLogOperator(OperatorBase):
@@ -9,22 +8,14 @@ class UploadLogOperator(OperatorBase):
         super().__init__(name=name)
         self.log_path = log_path
         self.verbose = verbose
-       
-        # write to aws cloud watch
-        self.aws_cloud_watch = CloudWatch('chunkflow')
         self.storage = Storage(log_path)
 
     def __call__(self, log, output_bbox):
         if self.verbose:
-            print('save log {} to {}'.format(output_bbox.to_filename(), 
+            print('save log {} to {}'.format(output_bbox.to_filename(),
                                              self.log_path))
- 
-        self.aws_cloud_watch.put_metric_data(log)
-
-        log_text = json.dumps(log)
-        
         # write to google cloud storage 
         self.storage.put_file(
             file_path=output_bbox.to_filename() + '.json',
-            content=log_text,
+            content=json.dumps(log),
             content_type='application/json')
