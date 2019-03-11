9b928cb017d7c089d233d41d6a8f6ea7a5e05151
jingpengw <jingpeng.wu@gmail.com>
2019-03-11 14:20:31 -0400

    fix benchmark script

--- bin/benchmark.py
+++ bin/benchmark.py
@@ -10,18 +10,24 @@ import pandas as pd
 from tqdm import tqdm
 
 dfs = []
+skiped_num = 0
 
 for file_name in tqdm(os.listdir('log'), desc='loading log files'):
     complete_file_name = os.path.join('log/', file_name)
 #     print('file name: {}'.format(complete_file_name))
     with open(complete_file_name) as f:
         d = json.load(f)
+
+    if 'compute_device' not in d:
+        # the mask is all black, most of the operations were skiped
+        skiped_num += 1
+        continue
+
     timing = d['timer']
     
     time_per_task = 0.0
     for t in d['timer'].values():
         time_per_task += t
-    
     item = pd.DataFrame({
         'compute_device': d['compute_device'],
         'inference': timing['inference'],
@@ -35,6 +41,7 @@ df = pd.concat(dfs)
 
 grouped_df = df.groupby('compute_device')
 
+print('\n There are {} tasks that have black mask and most of operations were skipped.'.format(skiped_num))
 
 print('\n\nmax time:')
 print(grouped_df.max()) 
