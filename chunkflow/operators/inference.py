1721fee7151a1479ee64cdf7f6f5225776f42719
Jingpeng Wu <jingpeng.wu@gmail.com>
2019-07-23 21:23:55 -0400

    No align inference (#78)
    * remove requirements.txt; make no aligned inference work
    
    * fix check input size function bug
    
    * fix parameter bug
    
    * fix batch size bug
    
    * all tests passed, but I don't know why I should create the offset list like that.
    
    * use slice list to reduce code redundency
    
    * I understand why the end should minus patch overlap now!
    
    * use pytest to replace unittest
    
    * add test for non-aligned inference and fix non-aligned inference bugs
    
    * fix coverage run
    
    * add __init__.py files in tests
    
    * fix travis test
    
    * fix a travis bug
    
    * add command line option
    
    * fix option bug
    
    * fig bug

--- chunkflow/operators/inference.py
+++ chunkflow/operators/inference.py
@@ -1,4 +1,5 @@
 from .operator_base import OperatorBase
+from .block_inference.block_inference_engine import BlockInferenceEngine
 
 
 class InferenceOperator(OperatorBase):
@@ -14,61 +15,76 @@ class InferenceOperator(OperatorBase):
                  framework: str='identity',
                  batch_size: int=1,
                  bump: str='wu',
+                 mask_output_chunk: bool=False,
                  verbose: bool=True, name: str='inference'):
 
         super().__init__(name=name, verbose=verbose)
-        
+
+        self.convnet_model = convnet_model
+        self.convnet_weight_path = convnet_weight_path
+        self.patch_size = patch_size
+        self.output_key = output_key
+        self.num_output_channels = num_output_channels
+        self.original_num_output_channels = original_num_output_channels
+        self.patch_overlap = patch_overlap
         self.framework = framework
+        self.batch_size = batch_size
+        self.bump = bump
+        self.mask_output_chunk = mask_output_chunk
 
         # mask in cpu in default
         mask_in_device = False
 
-        # prepare for inference
-        from .block_inference.block_inference_engine \
-            import BlockInferenceEngine
-        if framework == 'pznet':
-            from .block_inference.frameworks.pznet_patch_inference_engine \
-                import PZNetPatchInferenceEngine
-            patch_engine = PZNetPatchInferenceEngine(convnet_model, convnet_weight_path)
-        elif framework == 'pytorch':
-            from .block_inference.frameworks.pytorch_patch_inference_engine \
-                import PytorchPatchInferenceEngine
-            patch_engine = PytorchPatchInferenceEngine(
-                convnet_model,
-                convnet_weight_path,
-                output_key=output_key,
-                num_output_channels=num_output_channels,
-                device=device)
-        elif framework == 'pytorch-multitask':
-            # currently only this type of task support mask in device
-            mask_in_device = True
-            from .block_inference.frameworks.pytorch_multitask_patch_inference \
-                import PytorchMultitaskPatchInferenceEngine
-            patch_engine = PytorchMultitaskPatchInferenceEngine(
-                convnet_model,
-                convnet_weight_path,
-                patch_size=patch_size,
-                output_key=output_key,
-                patch_overlap=patch_overlap,
-                original_num_output_channels=original_num_output_channels,
-                num_output_channels=num_output_channels,
-                bump=bump)
-        elif framework == 'identity':
-            from .block_inference.frameworks.identity_patch_inference_engine \
-                import IdentityPatchInferenceEngine
-            patch_engine = IdentityPatchInferenceEngine(num_output_channels=3)
-        else:
-            raise Exception('invalid inference backend: {}'.format(framework))
-        
+        self._prepare_patch_engine()
+       
         self.block_inference_engine = BlockInferenceEngine(
-            patch_inference_engine=patch_engine,
+            patch_inference_engine=self.patch_engine,
             patch_size=patch_size,
             patch_overlap=patch_overlap,
             output_key=output_key,
             num_output_channels=num_output_channels,
             batch_size=batch_size,
             mask_in_device=mask_in_device,
+            mask_output_chunk=mask_output_chunk,
             verbose=verbose)
+         
+    def _prepare_patch_engine(self):
+        # prepare for inference
+        if self.framework == 'pznet':
+            from .block_inference.frameworks.pznet_patch_inference_engine \
+                import PZNetPatchInferenceEngine
+            self.patch_engine = PZNetPatchInferenceEngine(
+                self.convnet_model, self.convnet_weight_path)
+        elif self.framework == 'pytorch':
+            from .block_inference.frameworks.pytorch_patch_inference_engine \
+                import PytorchPatchInferenceEngine
+            self.patch_engine = PytorchPatchInferenceEngine(
+                self.convnet_model,
+                self.convnet_weight_path,
+                output_key=self.output_key,
+                num_output_channels=self.num_output_channels,
+                device=self.compute_device)
+        elif self.framework == 'pytorch-multitask':
+            # currently only this type of task support mask in device
+            mask_in_device = True
+            from .block_inference.frameworks.pytorch_multitask_patch_inference \
+                import PytorchMultitaskPatchInferenceEngine
+            self.patch_engine = PytorchMultitaskPatchInferenceEngine(
+                self.convnet_model,
+                self.convnet_weight_path,
+                patch_size=self.patch_size,
+                output_key=self.output_key,
+                patch_overlap=self.patch_overlap,
+                original_num_output_channels=self.original_num_output_channels,
+                num_output_channels=self.num_output_channels,
+                bump=self.bump)
+        elif self.framework == 'identity':
+            from .block_inference.frameworks.identity_patch_inference_engine \
+                import IdentityPatchInferenceEngine
+            self.patch_engine = IdentityPatchInferenceEngine(
+                num_output_channels=self.num_output_channels)
+        else:
+            raise Exception('invalid inference backend: {}'.format(self.framework))
  
     def __call__(self, chunk):
         # the chunk size should always be the same 
