93fb8213440865ab0270586247149f4267da3f18
jingpengw <jingpeng.wu@gmail.com>
2019-08-13 15:15:29 -0400

    fix patch model and size order

--- chunkflow/chunk/image/convnet_inference/patch_engine/pytorch.py
+++ chunkflow/chunk/image/convnet_inference/patch_engine/pytorch.py
@@ -9,10 +9,10 @@ from chunkflow.lib import load_source
 
 class PyTorch(PatchEngine):
     def __init__(self,
-                 model_file_name: str,
-                 weight_file_name: str,
                  patch_size: Union[tuple, list],
                  patch_overlap: Union[tuple, list],
+                 model_file_name: str,
+                 weight_file_name: str,
                  use_batch_norm: bool = True,
                  is_static_batch_norm: bool = False,
                  output_key: str = 'affinity',
