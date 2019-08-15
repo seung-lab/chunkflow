d712fe765c8c9954fa914b5bd69ce2b2d6d35d8a
Jingpeng Wu <jingpeng.wu@gmail.com>
2019-08-15 10:18:42 -0400

    Click callback (#111)
    * fix pytorch name
    
    * fix patch model and size order
    
    * fix pytorch name
    
    * more documentation; use @wrap to replace update_wrapper function
    
    * add port option for neuroglancer and move the voxel size to operator
    
    * add write-tif operator; add neuroglancer support for cleft map; fix pytorch inference;
    
    * rename test_main to test_flow to be consistent
    
    * rename save-images operator to save-pngs to be more explicit
    
    * fix operator
    
    * add test for tif io

--- tests/flow/test_read_write.py
+++ tests/flow/test_read_write.py
@@ -2,16 +2,17 @@ import unittest
 import numpy as np
 import os
 import shutil
-from time import sleep
 
 from chunkflow.chunk import Chunk
 
 from chunkflow.flow.read_h5 import ReadH5Operator
 from chunkflow.flow.write_h5 import WriteH5Operator
-from chunkflow.flow.save_images import SaveImagesOperator
+from chunkflow.flow.save_pngs import SavePNGsOperator
+from chunkflow.flow.read_tif import ReadTIFOperator
+from chunkflow.flow.write_tif import WriteTIFOperator
 
 
-def perform_io(chunk):
+def read_write_h5(chunk):
     assert isinstance(chunk, np.ndarray)
     file_name = 'test.h5'
     if os.path.exists(file_name):
@@ -23,10 +24,25 @@ def perform_io(chunk):
     assert chunk.global_offset == chunk2.global_offset
     os.remove(file_name)
 
+
+def read_write_tif(chunk):
+    """We'll lost global offset information using tif format!"""
+    assert isinstance(chunk, np.ndarray)
+    file_name = 'test.tif'
+    if os.path.exists(file_name):
+        os.remove(file_name)
+
+    WriteTIFOperator()(chunk, file_name)
+    chunk2 = ReadTIFOperator()(file_name)
+    assert np.alltrue(chunk == chunk2)
+    os.remove(file_name)
+
+
+def save_pngs(chunk):
     # test save images
     output_path = '/tmp/test/'
-    save_images_operator = SaveImagesOperator(output_path)
-    save_images_operator(chunk)
+    save_pngs_operator = SavePNGsOperator(output_path)
+    save_pngs_operator(chunk)
     print('remove the temporary directory.')
     shutil.rmtree(output_path)
 
@@ -36,13 +52,14 @@ class TestReadWrite(unittest.TestCase):
         print('test image io...')
         arr = np.random.randint(0, 256, size=(8, 16, 16), dtype=np.uint8)
         chunk = Chunk(arr, global_offset=(1, 2, 3))
-        perform_io(chunk)
+        read_write_h5(chunk)
+        read_write_tif(chunk)
 
     def test_read_write_aff(self):
         print('test affinitymap io...')
         arr = np.random.rand(3, 8, 16, 16).astype(np.float32)
         chunk = Chunk(arr, global_offset=(0, 1, 2, 3))
-        perform_io(chunk)
+        read_write_h5(chunk)
 
 
 if __name__ == '__main__':
