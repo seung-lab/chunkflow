953e2c4fbd0d9146e8dec2e1fd009a67fcb16901
jingpengw <jingpeng.wu@gmail.com>
2019-09-24 13:53:48 -0400

    add dust threshold and ids for sparse meshing

--- chunkflow/flow/mesh.py
+++ chunkflow/flow/mesh.py
@@ -4,6 +4,7 @@ import numpy as np
 from tqdm import tqdm
 
 from zmesh import Mesher
+import fastremap
 
 from cloudvolume import CloudVolume
 from cloudvolume.storage import Storage
@@ -19,6 +20,8 @@ class MeshOperator(OperatorBase):
                  voxel_size: tuple = (40, 4, 4),
                  simplification_factor: int = 100,
                  max_simplification_error: int = 8,
+                 dust_threshold: int = None,
+                 ids: set = None,
                  manifest: bool = False,
                  name: str = 'meshing',
                  verbose: bool = True):
@@ -35,6 +38,10 @@ class MeshOperator(OperatorBase):
             mesh simplification factor.
         max_simplification_error:
             maximum tolerance error of meshing.
+        dust_threshold:
+            do not mesh tiny objects with voxel number less than threshold
+        ids:
+            only mesh the selected segmentation ids, other segments will not be meshed.
         manifest:
             create manifest files or not. This should 
             not be True if you are only doing meshing for a segmentation chunk.
@@ -42,6 +49,8 @@ class MeshOperator(OperatorBase):
             operator name.
         verbose:
             print out informations or not.
+
+        Note that some functions are adopted from igneous.
         """
         super().__init__(name=name, verbose=verbose)
         self.simplification_factor = simplification_factor
@@ -50,6 +59,8 @@ class MeshOperator(OperatorBase):
         self.mesher = Mesher(voxel_size[::-1])
         self.output_path = output_path
         self.output_format = output_format
+        self.dust_threshold = dust_threshold
+        self.ids = ids
         self.manifest = manifest
 
         if manifest:
@@ -89,6 +100,28 @@ class MeshOperator(OperatorBase):
             raise NotImplementedError
         return data
 
+    def _remove_dust(self, seg: np.ndarray):
+        """
+        this function is adopted from igneous.
+        """
+        if self.dust_threshold or self.ids:
+            segids, voxel_nums = np.unique(seg, return_counts=True)
+            dust_segids = [sid for sid, ct in 
+                            zip(segids, voxel_nums) if ct < self.dust_threshold]
+
+            seg = fastremap.mask(seg, dust_segids, in_place=True)
+        return seg
+
+    def _only_keep_selected(self, seg: np.ndarray):
+        # precompute the remap function
+        if self.ids:
+            segids = set(np.unique(seg))
+            ids = self.ids.intersection( segids )
+            do_map = lambda x: x if x in ids else 0
+            map_func = np.vectorize(do_map)
+            seg = map_func(seg)
+        return seg
+
     def __call__(self, seg: np.ndarray):
         """Meshing the segmentation.
         
@@ -100,10 +133,20 @@ class MeshOperator(OperatorBase):
         assert isinstance(seg, np.ndarray)
         assert seg.ndim == 3
         assert np.issubdtype(seg.dtype, np.integer)
-        print('computing meshes from segmentation...')
+
+        if self.verbose:
+            print('remove dust segments')
+        seg = self._remove_dust(seg)
+        if self.verbose:
+            print('only keep selected segment ids, and remove others.')
+        seg = self._only_keep_selected(seg)
+
+        if self.verbose:
+            print('computing meshes from segmentation...')
         self.mesher.mesh(seg)
 
-        print('write mesh to storage...')
+        if self.verbose:
+            print('write mesh to storage...')
         with self.storage as stor:
             for obj_id in tqdm(self.mesher.ids(), desc='writing out meshes'):
                 data = self._get_mesh_data(obj_id)
