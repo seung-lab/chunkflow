1397efd4c331292d6ce35f5ace749358461d9f3f
jingpengw <jingpeng.wu@gmail.com>
2020-02-10 10:48:53 -0500

    add unit test for normalize section contrast operator

---
+++ tests/flow/test_normalize_section_contrast.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+np.random.seed(0)
+
+from chunkflow.chunk import Chunk
+from chunkflow.flow.normalize_section_contrast import NormalizeSectionContrastOperator
+
+
+def test_normalize_section_contrast():
+    DIR = os.path.join(os.path.dirname(__file__), '../data/levels')
+    #histogram = np.random.randint(10000, size=256, dtype=np.uint32)
+    image = np.arange(256, dtype=np.uint8).reshape(1, 16, 16)
+    image = Chunk(image, global_offset=(26774, 234, 456))
+    
+    levels_path = 'precomputed://file://' + DIR
+    operator = NormalizeSectionContrastOperator(levels_path, 1)
+    normalized = operator(image)
+    
+    normalized_filename = os.path.join(DIR, 'normalized.npz')
+    #np.savez( normalized_filename , normalized=normalized)
+    groundtruth = np.load(normalized_filename)['normalized']
+    np.testing.assert_array_equal(normalized, groundtruth)
