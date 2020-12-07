25cb2b917cd38c0d3e24e90e74fe214a63b86983
jingpengw <jingpeng.wu@gmail.com>
2020-12-07 09:17:48 -0500

    fix plugins test; use sharded mesh instead of separated mesh;

--- chunkflow/plugins/median_filter.py
+++ chunkflow/plugins/median_filter.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.ndimage import median_filter
 
 def exec(chunk: np.ndarray, size: tuple=(3,1,1), mode: str='reflect'):
-    return median_filter(chunk, size=size, mode=mode)
+    chunk = median_filter(chunk, size=size, mode=mode)
+    return [chunk]
 
     
