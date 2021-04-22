fc068f6a62e0978ea39e62442c7c840eead75fec
jingpengw <jingpeng.wu@gmail.com>
2021-04-22 10:25:10 -0400

    add some more info of median filter

--- chunkflow/plugins/median_filter.py
+++ chunkflow/plugins/median_filter.py
@@ -3,5 +3,6 @@ import numpy as np
 from scipy.ndimage import median_filter
 
 def execute(chunk: np.ndarray, size: tuple=(3,1,1), mode: str='reflect'):
+    print('median filtering of chunk...')
     chunk = median_filter(chunk, size=size, mode=mode)
     return [chunk]
