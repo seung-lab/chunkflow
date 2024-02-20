d4f80d46e14c4b0daff4b8d3dddff386d40f0ffa
Erik Schomburg <eschomburg@flatironinstitute.org>
2024-02-20 12:40:22 -0500

    gaussian_filter plugin operates on a copy, so does not modify input

--- chunkflow/plugins/gaussian_filter.py
+++ chunkflow/plugins/gaussian_filter.py
@@ -1,20 +1,22 @@
+import numpy as np
 from chunkflow.chunk import Chunk
 
 from scipy.ndimage import gaussian_filter
 
 
 def execute(chunk: Chunk, sigma: float=1.):
+    filtered = chunk.clone()
     for z in range(chunk.shape[-3]):
         if chunk.ndim == 4:
             for channel in range(chunk.shape[0]):
-                chunk.array[channel, z,:,:] = gaussian_filter(
+                filtered.array[channel, z,:,:] = gaussian_filter(
                     chunk.array[channel,z,:,:], sigma=sigma)
         elif chunk.ndim == 3:
-            chunk.array[z,:,:] = gaussian_filter(
+            filtered.array[z,:,:] = gaussian_filter(
                 chunk.array[z,:,:], sigma=sigma)
         else:
             raise ValueError(f'only support 4 or 3d, but got {chunk.ndim}')
 
     # 2d processing in xy plane
-    # chunk.array = gaussian_filter(chunk.array, sigma=sigma)
-    return chunk
+    # filtered.array = gaussian_filter(chunk.array, sigma=sigma)
+    return filtered
