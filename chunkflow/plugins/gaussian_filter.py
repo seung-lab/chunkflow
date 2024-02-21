88e8a9f4ef617ca46c29ea4e15771a63594c31cb
Erik Schomburg <eschomburg@flatironinstitute.org>
2024-02-21 13:56:13 -0500

    make inplace modification an option in gaussian_filter plugin

--- chunkflow/plugins/gaussian_filter.py
+++ chunkflow/plugins/gaussian_filter.py
@@ -4,19 +4,20 @@ from chunkflow.chunk import Chunk
 from scipy.ndimage import gaussian_filter
 
 
-def execute(chunk: Chunk, sigma: float=1.):
-    filtered = chunk.clone()
+def execute(chunk: Chunk, sigma: float=1., inplace=False):
+    if not inplace:
+        chunk = chunk.clone()
     for z in range(chunk.shape[-3]):
         if chunk.ndim == 4:
             for channel in range(chunk.shape[0]):
-                filtered.array[channel, z,:,:] = gaussian_filter(
+                chunk.array[channel, z,:,:] = gaussian_filter(
                     chunk.array[channel,z,:,:], sigma=sigma)
         elif chunk.ndim == 3:
-            filtered.array[z,:,:] = gaussian_filter(
+            chunk.array[z,:,:] = gaussian_filter(
                 chunk.array[z,:,:], sigma=sigma)
         else:
             raise ValueError(f'only support 4 or 3d, but got {chunk.ndim}')
 
     # 2d processing in xy plane
-    # filtered.array = gaussian_filter(chunk.array, sigma=sigma)
-    return filtered
+    # chunk.array = gaussian_filter(chunk.array, sigma=sigma)
+    return chunk
